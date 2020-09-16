b4764395b84d97adcbb5dfdd4a731852b3175655
mmdapl <2237221210@qq.com>
2020-09-17 00:17:18 +0800

    完成git自动更新脚本

--- git_update.sh
+++ git_update.sh
@@ -2,12 +2,16 @@
 ## git 脚本更新
 
 message=${1}
-
-echo '------start shell to  check  JavaScriptCollection update -----'
+## 获取当前时间
+time=$(date "+%Y-%m-%d %H:%M:%S")
+echo  '--'${time}' : 【command line:status】start shell to  check  JavaScriptCollection repository update --'
 git status
 
+echo  '--'${time}' : 【command line:add】 add changes  to the local --'
 git add .
 
+echo  '--'${time}' : 【command line:commit】  commit changes to the local -- '
 git commit -m ${message}
 
+echo  '--'${time}' : 【command line:push】push all to git repository online --'
 git push origin master
