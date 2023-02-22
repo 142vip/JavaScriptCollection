9e2f9bfeea919317eb7f4a4a8e9eb41474976aa8
Chu Fan <fairy_vip@2925.com>
2023-02-22 20:05:02 +0800

    feat(eslint): husky代码校验，eslint规则配置

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
+  },
+  plugins: [
+    'vue',
+    '@typescript-eslint'
+  ],
+  rules: {
+    'no-unused-vars': 0
+  }
+}
