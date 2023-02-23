b3be04fd41dc4a8f11c7a1179e3da8c981bf7ca6
Chu Fan <fairy_vip@2925.com>
2023-02-23 17:08:41 +0800

    style(code): 侧边栏配置优化

--- docs/read-books/read-books.sidebar.ts
+++ docs/read-books/read-books.sidebar.ts
@@ -1,84 +1,84 @@
 export const readBooksSidebar = [
-    {
-        text: '技术类',
+  {
+    text: '技术类',
+    children: [
+      {
+        text: 'ES6标准入门',
+        collapsible: false,
         children: [
-            {
-                text: 'ES6标准入门',
-                collapsible: false,
-                children: [
-                    {
-                        text: "简介",
-                        link: '/read-books/cs-books/ES6标准入门/简介.md'
-                    },
-                    {
-                        text: "let和const",
-                        link: '/read-books/cs-books/ES6标准入门/let和const.md'
-                    },
-                    {
-                        text: "解构赋值",
-                        link: '/read-books/cs-books/ES6标准入门/解构赋值.md'
-                    },
-                    {
-                        text: "字符串",
-                        link: '/read-books/cs-books/ES6标准入门/字符串.md'
-                    },
-                    {
-                        text: "正则表达式",
-                        link: '/read-books/cs-books/ES6标准入门/正则表达式.md'
-                    },
-                    {
-                        text: "数值",
-                        link: '/read-books/cs-books/ES6标准入门/数值.md'
-                    },
-                    {
-                        text: "函数",
-                        link: '/read-books/cs-books/ES6标准入门/函数.md'
-                    },
-                    {
-                        text: "数组",
-                        link: '/read-books/cs-books/ES6标准入门/数组.md'
-                    },
-                    {
-                        text: "对象",
-                        link: '/read-books/cs-books/ES6标准入门/对象.md'
-                    },
-                ]
+          {
+            text: '简介',
+            link: '/read-books/cs-books/ES6标准入门/简介.md'
+          },
+          {
+            text: 'let和const',
+            link: '/read-books/cs-books/ES6标准入门/let和const.md'
+          },
+          {
+            text: '解构赋值',
+            link: '/read-books/cs-books/ES6标准入门/解构赋值.md'
+          },
+          {
+            text: '字符串',
+            link: '/read-books/cs-books/ES6标准入门/字符串.md'
+          },
+          {
+            text: '正则表达式',
+            link: '/read-books/cs-books/ES6标准入门/正则表达式.md'
+          },
+          {
+            text: '数值',
+            link: '/read-books/cs-books/ES6标准入门/数值.md'
+          },
+          {
+            text: '函数',
+            link: '/read-books/cs-books/ES6标准入门/函数.md'
+          },
+          {
+            text: '数组',
+            link: '/read-books/cs-books/ES6标准入门/数组.md'
+          },
+          {
+            text: '对象',
+            link: '/read-books/cs-books/ES6标准入门/对象.md'
+          }
+        ]
 
-            },
-            {
-                text: '狼书',
-                collapsible: true,
-                link: '/read-books/cs-books/狼书.md'
-            },
-            {
-                text: '了不起的Node.js',
-                link: '/read-books/cs-books/了不起的Node.js.md'
-            },
-            {
-                text: '深入浅出Node.js',
-                link: '/read-books/cs-books/深入浅出的Node.js.md'
-            },
-            {
-                text: "分布式消息中间件实践",
-                link: "/read-books/cs-books/分布式消息中间件实践.md"
-            },
-            {
-                text: '高性能Web服务器详解',
-                link: '/read-books/cs-books/nginx'
-            }
-        ]
-    },
-    {
-        text: '非技术类',
-        children: [
-            {
-                text: '你好，孤独',
-                link: '/'
-            },
-            {
-                text: '测试',
-                link: '/f111'
-            }
-        ]
-    }
+      },
+      {
+        text: '狼书',
+        collapsible: true,
+        link: '/read-books/cs-books/狼书.md'
+      },
+      {
+        text: '了不起的Node.js',
+        link: '/read-books/cs-books/了不起的Node.js.md'
+      },
+      {
+        text: '深入浅出Node.js',
+        link: '/read-books/cs-books/深入浅出的Node.js.md'
+      },
+      {
+        text: '分布式消息中间件实践',
+        link: '/read-books/cs-books/分布式消息中间件实践.md'
+      },
+      {
+        text: '高性能Web服务器详解',
+        link: '/read-books/cs-books/nginx'
+      }
+    ]
+  },
+  {
+    text: '非技术类',
+    children: [
+      {
+        text: '你好，孤独',
+        link: '/'
+      },
+      {
+        text: '测试',
+        link: '/f111'
+      }
+    ]
+  }
 ]
