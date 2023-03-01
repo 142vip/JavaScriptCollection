88b70aad92b365b2bce92062b867b6edb1e377cc
Chu Fan <fairy_vip@2925.com>
2023-03-01 10:14:32 +0800

    feat: 侧边栏配置更新

--- docs/manuscript/battle-interview/battle-interview.sidebar.ts
+++ docs/manuscript/battle-interview/battle-interview.sidebar.ts
@@ -1,36 +1,36 @@
 export const battleInterviewSidebar = [
-    {
-        text: '常见面试题',
-        link: 'interview-problems'
-    },
-    {
-        text: '面经分享',
-        link: 'introduce-share'
-    },
-    {
-        text: '工作机会',
-        children: [
-            {
-                text: '校招',
-                link: 'school-recruit'
-            },
-            {
-                text: '社招',
-                link: 'social-recruit'
-            }
-        ]
-    },
-    {
-        text: '小技巧',
-        children: [
-            {
-                text: '校招',
-                link: '/'
-            },
-            {
-                text: '社招',
-                link: '/'
-            }
-        ]
-    }
+  {
+    text: '常见面试题',
+    link: 'interview-problems'
+  },
+  {
+    text: '面经分享',
+    link: 'introduce-share'
+  },
+  {
+    text: '工作机会',
+    children: [
+      {
+        text: '校招',
+        link: 'school-recruit'
+      },
+      {
+        text: '社招',
+        link: 'social-recruit'
+      }
+    ]
+  },
+  {
+    text: '小技巧',
+    children: [
+      {
+        text: '校招',
+        link: '/'
+      },
+      {
+        text: '社招',
+        link: '/'
+      }
+    ]
+  }
 ]
--- docs/manuscript/job-chance/job-chance.sidebar.ts
+++ docs/manuscript/job-chance/job-chance.sidebar.ts
@@ -1,9 +1,9 @@
 export const jobChanceSidebar = [
-    {
-        text: '工作机会',
-        children: [{
-            text: '字节跳动',
-            link: 'job-poster-bytedance'
-        }]
-    }
+  {
+    text: '工作机会',
+    children: [{
+      text: '字节跳动',
+      link: 'job-poster-bytedance'
+    }]
+  }
 ]
