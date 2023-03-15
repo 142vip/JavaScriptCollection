b6c2fb4a3048a5c7dbc6731463a79b2f67aac791
Chu Fan <fairy_vip@2925.com>
2023-03-16 00:04:07 +0800

    docs: 新增curl、vim等相关文档

--- docs/manuscript/solo-algorithm/solo-algorithm.sidebar.ts
+++ docs/manuscript/solo-algorithm/solo-algorithm.sidebar.ts
@@ -1,32 +1,32 @@
 export const soloAlgorithmSidebar = [
-    {
-        text: '面试必须刷101',
-        link: 'interview-101'
-    },
-    {
-        text: '剑指Offer',
-        link: 'sword-point'
-    },
-    {
-        text: 'Shell篇',
-        link: 'shell/'
-    },
-    {
-        text: '在线刷题',
-        children: [
+  {
+    text: '面试必须刷101',
+    link: 'interview-101'
+  },
+  {
+    text: '剑指Offer',
+    link: 'sword-point'
+  },
+  {
+    text: 'Shell篇',
+    link: 'shell/'
+  },
+  {
+    text: '在线刷题',
+    children: [
 
-            {
-                text: '牛客网',
-                link: 'https://www.nowcoder.com/'
-            },
-            {
-                text: 'LeetCode',
-                link: 'https://leetcode-cn.com/'
-            },
-            {
-                text: '杭电OJ',
-                link: 'http://acm.hdu.edu.cn/'
-            }
-        ]
-    }
+      {
+        text: '牛客网',
+        link: 'https://www.nowcoder.com/'
+      },
+      {
+        text: 'LeetCode',
+        link: 'https://leetcode-cn.com/'
+      },
+      {
+        text: '杭电OJ',
+        link: 'http://acm.hdu.edu.cn/'
+      }
+    ]
+  }
 ]
