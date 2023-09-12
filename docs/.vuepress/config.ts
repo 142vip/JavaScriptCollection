0724b3b08a723601642eee238310cc0ef6508c8c
chufan <fairy0115@2925.com>
2023-09-12 09:19:46 +0800

    feat: 站点新增百度统计相关配置

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -21,6 +21,17 @@ export default defineUserConfig({
         // vercel统计 相关配置
         [
             'script', {type: 'text/javascript', src: '/_vercel/insights/script.js'}
+        ],
+        // 百度统计
+        [
+        'script', {}, `
+            var _hmt = _hmt || [];
+            (function() {
+                var hm = document.createElement("script");
+                hm.src = "https://hm.baidu.com/hm.js?613c9d7af9e1c9a7f9eef6a55aa2399d";
+                var s = document.getElementsByTagName("script")[0];
+                s.parentNode.insertBefore(hm, s);
+            })();`
         ]
     ],
     markdown: {
