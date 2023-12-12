be7425f61938942d67463aa598ded71fa5b4b072
142vip.cn <fairy_vip@2925.com>
2023-12-12 10:10:25 +0800

    chore: update

---
+++ docs/manuscripts/server-end/typescript/typescript.sidebar.ts
@@ -0,0 +1,40 @@
+export const TypescriptSidebar = [
+  {
+    text: '基础教程',
+    prefix: '基础教程',
+    children: [
+      {
+        text: '快速入门',
+        link: '快速入门.md'
+      },
+      {
+        text: '基础类型',
+        link: '基础类型.md'
+      },
+      {
+        text: '变量声明',
+        link: '变量声明.md'
+      },
+      {
+        text: '接口',
+        link: '接口.md'
+      },
+      {
+        text: '泛型',
+        link: '泛型.md'
+      },
+      {
+        text: '类',
+        link: '类.md'
+      },
+      {
+        text: '高级类型',
+        link: '高级类型.md'
+      },
+      {
+        text: '装饰器',
+        link: '装饰器.md'
+      }
+    ]
+  }
+]
