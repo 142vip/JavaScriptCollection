4a883acf0a9584d99c827b51a42c9628f839d756
chufan <mmdapl@163.com>
2024-08-01 15:10:56 +0800

    feat: 全局搜索框支持中文提示

--- docs/.vuepress/theme/i18n.ts
+++ docs/.vuepress/theme/i18n.ts
@@ -1,11 +1,12 @@
 import type { ThemeLocaleData } from 'vuepress-theme-hope'
+import type { SearchProLocaleData } from 'vuepress-plugin-search-pro'
 import { NotFoundMsgList } from './constant'
 
 /**
  * 支持中文
  * 参考：https://theme-hope.vuejs.press/zh/config/i18n.html
  */
-const localCN: ThemeLocaleData = {
+const themeLocalCN: ThemeLocaleData = {
   lang: 'zh-CN',
   navbarLocales: {
     langName: '简体中文',
@@ -76,5 +77,26 @@ const localCN: ThemeLocaleData = {
  *   自定义语言，支持文案
  */
 export const i18n = {
-  '/': localCN,
+  '/': themeLocalCN,
+}
+
+/**
+ * 搜索框支持中文
+ * 参考：https://plugin-search-pro.vuejs.press/zh/config.html#locales
+ */
+export const searchProCNLocals: SearchProLocaleData = {
+  placeholder: '请输入需要搜索的关键字',
+  search: '搜索文档',
+  searching: '搜索中',
+  cancel: '取消',
+  select: '选择',
+  navigate: '切换',
+  exit: '关闭',
+  loading: '正在努力搜索中...',
+  queryHistory: '搜素文字历史',
+  resultHistory: '搜索结果',
+  emptyHistory: '没有搜索历史',
+  emptyResult: '无法找到相关结果',
+  defaultTitle: '搜索文档',
+  autocomplete: '自动补齐',
 }
