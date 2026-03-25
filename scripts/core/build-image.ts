39762475a73ef154000b64b10634553e8bff0700
chufan <mmdapl@163.com>
2026-03-25 11:04:03 +0800

    ci: 升级`@142vip/utils`依赖，调整镜像构建流程，增加hash标签

--- scripts/core/build-image.ts
+++ scripts/core/build-image.ts
@@ -4,39 +4,42 @@ import { VipDocker, VipGit, VipNodeJS, VipPackageJSON } from '@142vip/utils'
  * 功能：构建Docker镜像
  */
 async function buildImageMain(): Promise<void> {
-  try {
-    // 获取package.json文件
-    const { name, version, description } = VipPackageJSON.getPackageJSON<{ description: string }>()
+  // 获取package.json文件
+  const { name, version, description } = VipPackageJSON.getPackageJSON<{ description: string }>()
 
-    // 镜像地址
-    const imageName = `${OPEN_SOURCE_ADDRESS.DOCKER_ALIYUNCS_VIP}/docs:${name}-${version}`
+  // 镜像地址
+  const imageName = `${OPEN_SOURCE_ADDRESS.DOCKER_ALIYUNCS_VIP}/docs:${name}-${version}`
 
-    // 最近一次提交信息
-    const gitShortHash = VipGit.getRecentCommitShortHash()
+  // 最近一次提交信息
+  const gitShortHash = VipGit.getRecentCommitShortHash()
 
-    // 构建镜像
-    await VipDocker.buildImage({
-      imageName,
-      buildArgs: [
-        // 参数中是否包含 --proxy
-        ['NEED_PROXY', VipNodeJS.getProcessArgv().includes('--proxy')],
-        ['APP_NAME', name],
-        ['APP_VERSION', version],
-        ['APP_DESCRIPTION', description],
-        ['AUTHOR', OPEN_SOURCE_AUTHOR.name],
-        ['EMAIL', OPEN_SOURCE_AUTHOR.email],
-        ['HOME_PAGE', OPEN_SOURCE_AUTHOR.homePage],
-        ['GIT_HASH', gitShortHash],
-      ],
-      memory: 20000,
-      push: true,
-      delete: true,
-      logger: true,
-    })
+  let realImageName = imageName
+
+  // 如果version是否为预发布，是预发布则用hash当镜像名
+  if (VipGit.isPrerelease(version)) {
+    realImageName = `${imageName.split(':')[0]}:${name}-${version}-${gitShortHash}`
   }
-  catch (e) {
-    console.log('异常信息:', e)
-  }
+
+  // 构建镜像
+  await VipDocker.buildImage({
+    imageName: realImageName,
+    buildArgs: [
+      // 参数中是否包含 --proxy
+      ['NEED_PROXY', VipNodeJS.getProcessArgv().includes('--proxy')],
+      ['APP_NAME', name],
+      ['APP_VERSION', version],
+      ['APP_DESCRIPTION', description],
+      ['AUTHOR', OPEN_SOURCE_AUTHOR.name],
+      ['EMAIL', OPEN_SOURCE_AUTHOR.email],
+      ['HOME_PAGE', OPEN_SOURCE_AUTHOR.homePage],
+      ['GIT_HASH', gitShortHash],
+    ],
+    memory: 200000,
+    push: true,
+    delete: true,
+    logger: true,
+    progress: 'plain',
+  })
 }
 
 void buildImageMain()
